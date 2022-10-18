df98e34238cb928e9944764a27007ccd94814045
NAWELC <nawelchaib@hotmail.com>
2022-10-18 11:52:15 +0200

    first commit

---
+++ pages/home/index.jsx
@@ -0,0 +1,14 @@
+import Layout from "@src/components/layouts/layout";
+import Content from "@pages/home/content";
+
+
+const HomePage = () => {
+    return (
+        <>
+        <Layout children={<Content/>}/>
+        </>
+    );
+
+};
+
+export default HomePage;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+let styles = {
+    navbar: "flex flex-row"
+    
+    
+};
+
+const Navbar = () => {
+    return (
+         <nav>
+            <div>
+                <img src="https://images.pexels.com/photos/1549326/pexels-photo-1549326.jpeg" alt="auto"/>
+            </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="#">link1</a>
+                        </li>
+                        <li>
+                            <a href="#">link2</a>
+                        </li>
+                        <li>
+                            <a href="#">link3</a>
+                        </li>
+                    </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
